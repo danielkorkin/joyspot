e0c123df9a2693b5545f61d164c28862c1776181
Devium <108692136+devium335@users.noreply.github.com>
2024-06-09 14:52:03 -0400

    Revert "."
    This reverts commit 0ab0ab80859dcaab1b3a734251032cb4234d4113.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -20,37 +20,3 @@ export type Post = {
 		name: string;
 	};
 };
-
-// types.ts
-declare module "perspective-api-client" {
-	interface AnalyzeResult {
-		attributeScores: {
-			TOXICITY: {
-				spanScores: Array<{
-					begin: number;
-					end: number;
-					score: {
-						value: number;
-						type: string;
-					};
-				}>;
-				summaryScore: {
-					value: number;
-					type: string;
-				};
-			};
-		};
-		languages: string[];
-		detectedLanguages: string[];
-	}
-
-	interface PerspectiveOptions {
-		apiKey?: string;
-		authClient?: any;
-	}
-
-	export default class Perspective {
-		constructor(options: PerspectiveOptions);
-		analyze(text: string): Promise<AnalyzeResult>;
-	}
-}
