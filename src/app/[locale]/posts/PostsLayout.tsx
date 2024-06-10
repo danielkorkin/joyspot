ca8f858fc140e4c6f9020ca7f7fd10eebc25125d
Devium <108692136+devium335@users.noreply.github.com>
2024-06-10 19:48:01 -0400

    Skeleton

--- src/app/[locale]/posts/PostsLayout.tsx
+++ src/app/[locale]/posts/PostsLayout.tsx
@@ -3,7 +3,8 @@ import PostCard from "@/src/components/PostCard";
 import Navbar from "@/src/components/navbar";
 import { Post } from "@/src/lib/types";
 import { useTranslations } from "next-intl";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
+import { Suspense } from "react";
 
 interface PostsLayoutProps {
 	posts: Post[];
@@ -22,9 +23,24 @@ export const PostsLayout = ({ posts }: PostsLayoutProps) => {
 				<CreatePostDialog />
 			</div>
 			<div className="mt-2">
-				{posts.map((post) => (
-					<PostCard key={post.id} post={post} />
-				))}
+				<Suspense
+					fallback={
+						<div class="rounded-lg border bg-card text-card-foreground shadow-sm mx-20 my-8 animate-pulse">
+							<div class="flex flex-col space-y-1.5 p-6">
+								<div class="h-6 bg-gray-200 rounded"></div>
+								<div class="h-4 bg-gray-200 rounded w-2/3"></div>
+							</div>
+							<div class="p-6 pt-0">
+								<div class="h-6 bg-gray-200 rounded mb-2"></div>
+								<div class="h-6 bg-gray-200 rounded w-3/4"></div>
+							</div>
+						</div>
+					}
+				>
+					{posts.map((post) => (
+						<PostCard key={post.id} post={post} />
+					))}
+				</Suspense>
 			</div>
 			<Toaster />
 		</div>
